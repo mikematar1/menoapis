db4d4f2eec4461d8b822d2bc28ce822425d5bd74
mikematar1 <mike.matar97@gmail.com>
2023-06-04 23:57:15 +0300

    changes

--- src/database/migrations/2023_04_14_142346_update_deals_table.php
+++ src/database/migrations/2023_04_14_142346_update_deals_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Deals', function (Blueprint $table) {
+        Schema::table('deals', function (Blueprint $table) {
             $table->dropColumn('clicks');
             $table->string("currency");
         });
